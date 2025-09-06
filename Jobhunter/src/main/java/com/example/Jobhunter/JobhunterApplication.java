37f92d447c0ffbd6866edf3d67ebb08269e6daa5
Tín <Trungtin0701200@gmail.com>
2025-09-06 23:09:17 +0700

    Thêm code để disable security mặc định

--- Jobhunter/src/main/java/com/example/Jobhunter/JobhunterApplication.java
+++ Jobhunter/src/main/java/com/example/Jobhunter/JobhunterApplication.java
@@ -3,7 +3,8 @@ package com.example.Jobhunter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class JobhunterApplication {
 
 	public static void main(String[] args) {

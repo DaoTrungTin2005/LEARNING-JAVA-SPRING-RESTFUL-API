bd0712b5e40c97eb702d97dcd64ff3b8ee88654d
Tín <Trungtin0701200@gmail.com>
2025-09-14 10:05:28 +0700

    CUSTOMIZE cái message cho ra chuẩn : {     "statusCode": 201,     "error": null,     "message": "Call API success",     "data": {         "id": 17,         "name": "abc11",         "email": "abc@gamil.com",         "password": "123456"     } } (Đây là lúc create user mới , send từ postman)

---
+++ Jobhunter/src/main/java/com/example/Jobhunter/util/FormatRestResponse.java
@@ -0,0 +1,51 @@
+package com.example.Jobhunter.util;
+
+import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Servlet;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import com.example.Jobhunter.domain.RestResponse;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedConvertype,
+            Class selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response) {
+
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(status);
+
+        if (status >= 400) {
+            // Case error
+            return body;
+        } else {
+            // Case success
+            res.setData(body);
+            res.setMessage("Call API success");
+        }
+
+        return res;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+}
